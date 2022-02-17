364685085c16b99c2faef4dd1ab73488a64b7367
Edilene <edileneonogueira@gmail.com>
2022-02-17 10:35:41 -0300

    Primeiro exercício de medidas inserido projeto

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,45 @@
+// 1 - Pacote
+package intro;
+
+// 2 - Referência as bibliotecas
+
+// 3 - Classe
+public class Medidas {
+    // 3.1 Atributos - Caracteristicas
+
+    // 3.2 - Métodos e Funções(verbos o que sabe fazer)
+     public static void main(String[] args) {
+        System.out.println("    Calculo de Areas");
+         //Calculo de area reduzido
+         int largura = 4;
+         int comprimento = 3;
+
+         System.out.println("Para largura de " + largura + " e o comprimento de "
+                 + comprimento + "m, a area é de " + largura * comprimento + "m².");
+
+
+
+    }
+    public void calcularAreaModoExtenso(){
+//  Calculo de area = Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 7;  //largura recebe 4
+        comprimento = 3;
+
+        resultado = largura * comprimento;
+        System.out.println("Para largura de " + largura + " e o comprimento de "
+                + comprimento + "m, a area é de " + resultado + "m².");
+    }
+
+    public void calcularAreaModoCompacto(){
+         //Calculo de area reduzido
+         int largura = 4;
+         int comprimento = 3;
+
+        System.out.println("Para largura de " + largura + " e o comprimento de "
+                + comprimento + "m, a area é de " + largura * comprimento + "m².");
+    }
+}
